f755e747da9a67ff0c59df95c10448aaf97c5e9e
Evenrejn <brendivajn@gmail.com>
2021-07-22 19:44:05 +0300

    .

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,8 +2,46 @@ import React from "react";
 import styles from "./Users.module.css";
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        photoUrl: "#",
+        followed: false,
+        fullName: "Egor",
+        status: "Jarr",
+        location: { city: "Minsk", country: "Bel" },
+      },
+      {
+        id: 2,
+        photoUrl: "#",
+        followed: true,
+        fullName: "Vova",
+        status: "HW",
+        location: { city: "Msk", country: "Ru" },
+      },
+      {
+        id: 3,
+        photoUrl: "#",
+        followed: false,
+        fullName: "Ira",
+        status: "HarKun",
+        location: { city: "SPb", country: "Ru" },
+      },
+      {
+        id: 4,
+        photoUrl: "#",
+        followed: true,
+        fullName: "Tanya",
+        status: "Status)",
+        location: { city: "Elgsk", country: "Ru" },
+      },
+    ]);
+  }
+
   return (
     <div>
+      dghdth
       {/* {props.users.map((u) => (
         <div key={u.id}>
           <div>
@@ -16,6 +54,10 @@ let Users = (props) => {
             </div>
             <button>Follow</button>{}
           </div>
+          {u.followed 
+            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+            : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
+        }
         </div>{u.fullName}{u.status}{u.location.country}{u.location.city}{u.photoUrl}
       ))} */}
     </div>
