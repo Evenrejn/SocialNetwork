6c14a2d481a1ce9b32b4d7eb20b17f9a1fa5324e
Evenrejn <brendivajn@gmail.com>
2021-07-23 17:07:13 +0300

    .

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,107 @@
 import React from "react";
-import styles from "./Users.module.css";
+import s from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/default_avatar.png";
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: "#",
-        followed: false,
-        fullName: "Egor",
-        status: "Jarr",
-        location: { city: "Minsk", country: "Bel" },
-      },
-      {
-        id: 2,
-        photoUrl: "#",
-        followed: true,
-        fullName: "Vova",
-        status: "HW",
-        location: { city: "Msk", country: "Ru" },
-      },
-      {
-        id: 3,
-        photoUrl: "#",
-        followed: false,
-        fullName: "Ira",
-        status: "HarKun",
-        location: { city: "SPb", country: "Ru" },
-      },
-      {
-        id: 4,
-        photoUrl: "#",
-        followed: true,
-        fullName: "Tanya",
-        status: "Status)",
-        location: { city: "Elgsk", country: "Ru" },
-      },
-    ]);
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
+  // props.setUsers([
+  //   {
+  //     id: 1,
+  //     photoUrl: "http://placehold.it/40x40/",
+  //     followed: false,
+  //     fullName: "Egor",
+  //     status: "Jarr",
+  //     location: { city: "Minsk", country: "Bel" },
+  //   },
+  //   {
+  //     id: 2,
+  //     photoUrl: "http://placehold.it/40x40/",
+  //     followed: true,
+  //     fullName: "Vova",
+  //     status: "HW",
+  //     location: { city: "Msk", country: "Ru" },
+  //   },
+  //   {
+  //     id: 3,
+  //     photoUrl: "http://placehold.it/40x40/",
+  //     followed: false,
+  //     fullName: "Ira",
+  //     status: "HarKun",
+  //     location: { city: "SPb", country: "Ru" },
+  //   },
+  //   {
+  //     id: 4,
+  //     photoUrl: "http://placehold.it/40x40/",
+  //     followed: true,
+  //     fullName: "Tanya",
+  //     status: "Status)",
+  //     location: { city: "Elgsk", country: "Ru" },
+  //   },
+  // ]);
+  //}
 
   return (
-    <div>
-      dghdth
-      {/* {props.users.map((u) => (
-        <div key={u.id}>
-          <div>
-            <div>
-              <img />
+    <div className={s["main-content"]}>
+      <h2>Users</h2>
+      {props.users.map((u) => (
+        <div key={u.id} className={s["wrap-users"]}>
+          <div className={s["users__user-item"]}>
+            <div className={s["users__user-item-main"]}>
+              <div>
+                <img
+                  className={s["users__user-item-img"]}
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  alt="L"
+                />
+              </div>
+              {u.followed ? (
+                <button
+                  className={s["users__user-item-follow"]}
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  className={s["users__user-item-follow"]}
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
             </div>
-            <button>Follow</button>
-            <div>
-              <img />
-            </div>
-            <button>Follow</button>{}
           </div>
-          {u.followed 
-            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-            : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
-        }
-        </div>{u.fullName}{u.status}{u.location.country}{u.location.city}{u.photoUrl}
-      ))} */}
+          <div className={s["users__info"]}>
+            <div className={s["wrapper-user"]}>
+              <p className={s["users__info-name"]}>{u.name}</p>
+              <p className={s["users__info-status"]}>{u.status}</p>
+            </div>
+            <div className={s["wrapper-user"]}>
+              <p className={s["users__info-location"]}>
+                {"u.location.country"}
+                <br></br>
+                {"u.location.city"}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button className={s["users__upload-more"]} onClick={getUsers}>
+        Show more
+      </button>
     </div>
   );
 };
